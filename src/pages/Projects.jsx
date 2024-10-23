76c0d6675e402f2db5ac8979eb2f6d670bd10a39
Maria Peters <mp-developer@outlook.com>
2024-10-23 10:59:54 -0600

    try again

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FaGithub } from 'react-icons/fa';
 import MyOrders from '../images/MyOrders.webp';
-import shopLogo from '../images/ShopLogo.png';
+import shopLogo from '../images/ShopLogo.webp';
 import MyLogo from '../images/MyLogo.webp';
 import Truss from '../images/TrussProject.png';
 
