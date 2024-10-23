a6804a5068c180594c890b4c7a72538c1ea8dd34
Maria Peters <mp-developer@outlook.com>
2024-10-23 12:14:08 -0600

    added a project card

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { FaGithub } from 'react-icons/fa';
-import MyOrders from '../images/MyOrders.webp';
-import shopLogo from '../images/shopLogo.webp';
-import MyLogo from '../images/MyLogo.webp';
-import Truss from '../images/TrussProject.png';
+import React from "react";
+import { FaGithub } from "react-icons/fa";
+import MyOrders from "../images/MyOrders.webp";
+import shopLogo from "../images/shopLogo.webp";
+import MyLogo from "../images/MyLogo.webp";
+import Truss from "../images/TrussProject.png";
+import PrimeFinishLogo from "../images/PrimeFinishLogo.png";
 
 const Projects = () => {
   return (
@@ -31,7 +32,8 @@ const Projects = () => {
           </div>
           <h2>Tamale Order Tracker</h2>
           <p>
-            The Tamale Order Tracker app helps manage food orders easily, with the ability to print PDF lists for pickup.
+            The Tamale Order Tracker app helps manage food orders easily, with
+            the ability to print PDF lists for pickup.
           </p>
           <div className="flex justify-center space-x-4">
             <a
@@ -67,7 +69,8 @@ const Projects = () => {
           </div>
           <h2>Building Shops Estimator</h2>
           <p>
-            Helps contractors quickly calculate project costs and generate material lists.
+            Helps contractors quickly calculate project costs and generate
+            material lists.
           </p>
           <div className="flex justify-center space-x-4">
             <a
@@ -103,7 +106,8 @@ const Projects = () => {
           </div>
           <h2>Portfolio Website</h2>
           <p>
-            Built with React and Tailwind CSS, showcasing projects and technical skills.
+            Built with React and Tailwind CSS, showcasing projects and technical
+            skills.
           </p>
           <div className="flex justify-center space-x-4">
             <a
@@ -124,8 +128,44 @@ const Projects = () => {
             </a>
           </div>
         </div>
-
-        {/* Project 4: Truss Estimator Application */}
+        {/* Project 4: Prime Finishes Painting */}
+        <div className="card">
+          <div className="aspect-ratio-box">
+            <img
+              src={PrimeFinishLogo}
+              alt="Prime Finish Painting"
+              width="400"
+              height="300"
+              className="card-img"
+              loading="lazy"
+            />
+          </div>
+          <h2>Prime Finish Painting</h2>
+          <p>
+            This app is for creating invoices, estimates, tracking job progress,
+            managing expenses, and tracking contractor payments efficiently
+            for a painting company.
+          </p>
+          <div className="flex justify-center space-x-4">
+            <a
+              href="https://leap-app-alpha.vercel.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              View Project
+            </a>
+            <a
+              href="https://github.com/Marysgithub321/leap-jobs"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center space-x-2"
+            >
+              <FaGithub />
+              <span>View Code</span>
+            </a>
+          </div>
+        </div>
+        {/* Project 5: Truss Estimator Application */}
         <div className="card">
           <div className="aspect-ratio-box">
             <img
@@ -138,9 +178,7 @@ const Projects = () => {
             />
           </div>
           <h2>Truss Estimator Application</h2>
-          <p>
-            Calculates project costs and generates detailed PDF estimates.
-          </p>
+          <p>Calculates project costs and generates detailed PDF estimates.</p>
           <div className="flex justify-center space-x-4">
             <a
               href="https://1drv.ms/u/s!AvelyWS0bAEulpIRMSGwQNVZ1ZD4kw?e=Y5WJMC"
